8147a80632e6fdc02fa1f5df3fbb66401ae38e85
DESKTOP-I1BG23F\user <shalom419@naver.com>
2022-07-18 23:13:19 +0900

    Self Study Day 1 Short Cut

---
+++ Java Self Study/src/org/Java/Self/Study/JavaShortCut.java
@@ -0,0 +1,30 @@
+package org.Java.Self.Study;
+
+public class JavaShortCut {
+	public static void main(String[] args) {
+//	ctrl+shift+L 단축키
+//	ctrl+ +,- 폰트크기 증가/감소
+//	ctrl+D 한줄 삭제
+//	ctrl+alt+shift+Down 행단위 복사
+//	alt+shift+A 멀티컬럼 편집
+//	alt+up, down 행단위 이동
+//	ctrl+i 자동 들여쓰기
+//	ctrl+/ 주석 토글
+//	ctrl+space 자동완성
+//	ctrl+F11 실행
+		
+	System.out.println("ctrl+shift+L 단축키");
+	System.out.println("ctrl+ +,- 폰트크기 증가/감소");
+	System.out.println("ctrl+D 한줄 삭제");
+	System.out.println("ctrl+alt+shift+Down 행단위 복사");
+	System.out.println("alt+shift+A 멀티컬럼 편집");
+	System.out.println("alt+up, down 행단위 이동");
+	System.out.println("ctrl+i 자동 들여쓰기");
+	System.out.println("ctrl+/ 주석 토글");
+	System.out.println("ctrl+space 자동완성");
+	System.out.println("ctrl+F11 실행");
+		
+
+		
+	}
+}
