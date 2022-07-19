a2b92a753d1cff8fee3e707af1c4d6e2e4d913e2
DESKTOP-I1BG23F\user <kangharyeom@gmail.com>
2022-07-19 09:06:02 +0900

    Java Self Study Day2 - Activity 1
    How different with "println" and "print"

---
+++ JavaSelfStudyDay2/src/org/Java/Self/Study/Day2/Day2Activity1.java
@@ -0,0 +1,10 @@
+package org.Java.Self.Study.Day2;
+
+public class Day2Activity1 {
+	public static void main(String[] args) {
+		System.out.println("println은 작업 후에 줄바꿈");
+		System.out.print("print은 한줄로 나옴");
+		System.out.print("print은 한줄로 나옴");
+		System.out.print("print은 한줄로 나옴");
+	}
+}
