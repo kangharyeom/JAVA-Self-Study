fd78b9e3119dbe6cee293dd251d3e9b26831f6eb
DESKTOP-I1BG23F\user <kangharyeom@gmail.com>
2022-07-19 09:35:35 +0900

    Day2 Activity2 Korean Age Calculator

--- JavaSelfStudyDay2/src/org/Java/Self/Study/Day2/Day2Activity2.java
+++ JavaSelfStudyDay2/src/org/Java/Self/Study/Day2/Day2Activity2.java
@@ -3,10 +3,10 @@ package org.Java.Self.Study.Day2;
 class Day2Activity2 {
 	public static void main(String[] args) {
 		// 한국나이 계산기를 만들어보자
-				int MyAge = 0, ThisYear = 2022, BirthDay = 1995;
-				MyAge = ThisYear - BirthDay + 1;
-				System.out.print("나의 한국나이는 ");
-				System.out.print(MyAge);
-				System.out.print("세 입니다.");
-			}
+		int MyAge = 0, ThisYear = 2022, BirthDay = 1995;
+		MyAge = ThisYear - BirthDay + 1;
+		System.out.print("나의 한국나이는 ");
+		System.out.print(MyAge);
+		System.out.print("세 입니다.");
+		}
 }
