7f651762f035b68846a21fb039d0114974ba5656
DESKTOP-I1BG23F\user <kangharyeom@gmail.com>
2022-07-19 10:02:26 +0900

    Day2 Activity3 variable Final Literal

--- JavaSelfStudyDay2/src/org/Java/Self/Study/Day2/Day2Activity3.java
+++ JavaSelfStudyDay2/src/org/Java/Self/Study/Day2/Day2Activity3.java
@@ -2,11 +2,16 @@ package org.Java.Self.Study.Day2;
 
 class Day2Activity3 {
 	public static void main(String[] args) {
-// 한국나이 계산기를 만들어보자
-		int MyAge = 0, ThisYear = 2022, BirthDay = 1995;
-		MyAge = ThisYear - BirthDay + 1;
-		System.out.print("나의 한국나이는 ");
-		System.out.print(MyAge);
-		System.out.print("세 입니다.");
+// 변수 (ex: int = 100 ) = 하나의 값을 저장하기 위한 공간
+// 상수 (ex: final int = 100) = 한 번만 값을 저장 가능한 변수 
+// 리터럴	= 값을 의미하는 것
+		final int Max = 20;
+		int Score = 100;
+		Score = 200;
+		
+		System.out.println(Score+Max);
+// 변수를 선언한 이후에 재 선언을 하면 후기값으로 처리된다
+// final(상수)을 선언 한 이후에 값을 변경하면 에러가 발생한다.
+		
 	}
 }
