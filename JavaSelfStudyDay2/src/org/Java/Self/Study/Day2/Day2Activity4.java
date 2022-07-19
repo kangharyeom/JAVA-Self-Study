e75b86f06a1e8cf5b9f190e9430361c36fc9bf16
DESKTOP-I1BG23F\user <kangharyeom@gmail.com>
2022-07-19 18:55:02 +0900

    Day2 Activity4 how to use printf

--- JavaSelfStudyDay2/src/org/Java/Self/Study/Day2/Day2Activity4.java
+++ JavaSelfStudyDay2/src/org/Java/Self/Study/Day2/Day2Activity4.java
@@ -2,21 +2,21 @@ package org.Java.Self.Study.Day2;
 
 class Day2Activity4 {
 	public static void main(String[] args) {
-// 리터럴의 접두사와 접미사
-//	논리형 = false, true
-//	정수형 = L
-		long Long = 100L;
-		System.out.println(Long);
-//  실수형 = f,d
-		double Double = 3.141592d;
-		float Float = 1.1345f;
-		System.out.println(Double);
-		System.out.println(Float);
-//	문자형 = ''(작은 따옴표)
-		char ch = 'A';
-		System.out.println(ch);
-//	문자열 = ""(큰 따옴표)
-		String str = "ABC";
-		System.out.println(str);
+		System.out.println(10/3);
+//		정수/정수 이기 때문에 실수/정수로 바꿔줘야 한다.
+		System.out.println(10.0/3);
+		System.out.printf("%f\n", 10.0/3);
+//		%b = boolean 형식으로 출력
+//		%d = 10진 정수의 형식으로 출력
+//		%o = 8진 정수의 형식으로 출력
+//		%x, %X = 16진 정수의 형식으로 출력
+//		%f = 부동 소수점(floating-point)의 형식으로 출력
+//		%e, %E = 지수(exponent) 표현식의 형식으로 출력
+//		%c = 문자로 출력
+//		%s = 문자열로 출력
+//		%n = 줄바꿈 (=\n)
+		System.out.printf("%d%n", 15); // 10진수
+		System.out.printf("%o%n", 15); // 8진수
+		System.out.printf("%x%n", 15); // 16진수
 	}
 }
