8058fc00fbab9366b4075ccb43249a1a438c5998
DESKTOP-I1BG23F\user <kangharyeom@gmail.com>
2022-07-19 10:28:12 +0900

    Day2 Activity3 variable Final Literal

---
+++ JavaSelfStudyDay2/src/org/Java/Self/Study/Day2/Day2Activity4.java
@@ -0,0 +1,22 @@
+package org.Java.Self.Study.Day2;
+
+class Day2Activity4 {
+	public static void main(String[] args) {
+// 리터럴의 접두사와 접미사
+//	논리형 = false, true
+//	정수형 = L
+		long Long = 100L;
+		System.out.println(Long);
+//  실수형 = f,d
+		double Double = 3.141592d;
+		float Float = 1.1345f;
+		System.out.println(Double);
+		System.out.println(Float);
+//	문자형 = ''(작은 따옴표)
+		char ch = 'A';
+		System.out.println(ch);
+//	문자열 = ""(큰 따옴표)
+		String str = "ABC";
+		System.out.println(str);
+	}
+}
