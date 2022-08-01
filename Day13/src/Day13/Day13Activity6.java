e639955c12d51f523e4571de37cedd05fb114b1d
DESKTOP-I1BG23F\user <kangharyeom@gmail.com>
2022-08-01 14:57:00 +0900

    Day13 Activity6 형변환

---
+++ Day13/src/Day13/Day13Activity6.java
@@ -0,0 +1,32 @@
+package Day13;
+
+class Day13Activity6 {
+	public static void main(String[] args) {
+		Car car = new Car();
+		FireEngine fe2 = (FireEngine)car;
+		fe2.stop();
+	}
+}
+
+class Car{
+	String color;
+	int door;
+	
+	void drive() {
+		System.out.println("dirve");
+	}
+	void stop() {
+		System.out.println("stop");
+	}
+}
+class FireEngine extends Car{
+		String color;
+		int door;
+		
+		void drive() {
+			System.out.println("dirve2");
+		}
+		void stop() {
+			System.out.println("stop2");
+		}
+}
